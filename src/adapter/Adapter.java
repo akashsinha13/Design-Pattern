13b2a8fb158374d70475ef46c49fdb30d87485a2
Akash Sinha <sinhaakashak@gmail.com>
2021-06-17 22:41:22 +0530

    Added adapter pattern desc

---
+++ src/adapter/Adapter.java
@@ -0,0 +1,12 @@
+package adapter;
+
+/*Acts as man in the middle
+ It converts the request of the resource to the request accepted by the target.
+ Generally use with third party to change it as required.
+*/
+public class Adapter {
+    /*
+       It is straight forward. Take data in one form and return it in another form.
+       Example take full name and return first name.
+     */
+}
