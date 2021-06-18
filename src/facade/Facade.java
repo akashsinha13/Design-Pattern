fdc4ed2808c896ee27788c9d570a63ad5b0603b7
Akash Sinha <sinhaakashak@gmail.com>
2021-06-18 23:43:14 +0530

    Added Facade desc

---
+++ src/facade/Facade.java
@@ -0,0 +1,13 @@
+package facade;
+
+public class Facade {
+    /*
+      A facade is for hiding features/internal complexity from external clients
+      and to give them a unified access point to public functionality.
+
+      The best example is AppStore where we can order an app
+      and we do not have to know what happens in the background: the app is developed
+      and tested. And we are happy that we don’t have to call every phase of the development
+      life-cycle, just the one facade.
+    */
+}
